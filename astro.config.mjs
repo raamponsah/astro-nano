60cc44f86f0774e88e7bf8b95de83265e41dc92e
Raamponsah <raamponsah@gimpa.edu.gh>
2024-06-21 15:09:21 +0000

    astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,10 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
-
-// https://astro.build/config
 export default defineConfig({
   site: "my-site-h1zeo7roq-raamponsahs-projects.vercel.app",
   integrations: [mdx(), sitemap(), tailwind()],
+  outDir: 'dist',
   adapter: vercel({
     webAnalytics: { enabled: true }
   }),
