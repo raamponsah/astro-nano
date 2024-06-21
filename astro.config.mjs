20d01dfcb3448cf6b755ed1d2ca0fedcff899729
Raamponsah <raamponsah@gimpa.edu.gh>
2024-06-21 13:53:08 +0000

    update my site analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,14 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
 
+// https://astro.build/config
 export default defineConfig({
-  site: "https://astro-nano-demo.vercel.app",
+  site: "my-site-h1zeo7roq-raamponsahs-projects.vercel.app",
   integrations: [mdx(), sitemap(), tailwind()],
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  }),
+  output: "server"
 });
